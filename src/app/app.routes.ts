f7d34642e5b82c3662b249dbf86933f0dccacb17
k1llkmc <kokushiboxdd@gmail.com>
2024-10-23 23:20:28 +0500

    routes are kind of ready

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './routes/home/home.component';
+import { AboutComponent } from './routes/about/about.component';
+import { EventsComponent } from './routes/events/events.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: '',
+  component: HomeComponent,
+},
+{
+  path: 'about',
+  component: AboutComponent,
+},
+{
+  path: 'events',
+  component: EventsComponent,
+}
+];
